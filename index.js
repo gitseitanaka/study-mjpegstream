173140b88756fa90dac744a40ffe138f05e262f9
gitseitanaka <gitseitanaka@users.noreply.github.com>
2015-08-03 15:34:24 +0900

    * add disconnect action on front size.
    * organized the code.

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var fs = require('fs');
 var path = require('path');
 
 var RingPath = require('study-caddon-echo-string');
@@ -18,51 +17,48 @@ var sockets = {};
 var ringPaths = {};
 io.on('connection', function (socket) {
 	sockets[socket.id] = socket;
+	socket.emit('connected', socket.id);
+	console.log("count connected : ", Object.keys(sockets).length);
 
-	console.log("Total clients connected : ", Object.keys(sockets).length);
-	socket.on('disconnect', function () {
-		delete sockets[socket.id];
-		if (socket.id in ringPaths) {
-			ringPaths[socket.id].stop();
-		}
-		// no more sockets, kill the stream
-		if (Object.keys(sockets).length == 0) {
-			app.set('watchingFile', false);
-
-		}
-	});
 	socket.on('start-stream', function () {
 		startStreaming(io, sockets[socket.id]);
 	});
+
 	socket.on('stop-stream', function () {
 		stopStreaming(io, sockets[socket.id]);
 	});
+
+	socket.on('disconnect', function () {
+
+		if (socket.id in sockets) {
+			delete sockets[socket.id];
+		}
+		if (socket.id in ringPaths) {
+			ringPaths[socket.id].stop();
+			delete ringPaths[socket.id];
+		}
+	});
+
 });
 http.listen(3000, function () {
 	console.log('listening on *:3000');
 });
-function stopStreaming(aio, aSocket) {
-	if (Object.keys(sockets).length == 0) {
-		app.set('watchingFile', false);
 
-	}
+
+function stopStreaming(aio, aSocket) {
 	if (aSocket.id in ringPaths) {
 		ringPaths[aSocket.id].stop();
+		delete ringPaths[aSocket.id];
 	}
 }
+
 function startStreaming(aio, aSocket) {
-	if (app.get('watchingFile')) {
-//		return;
-	}
-
-	console.log('Watching for changes...');
-	app.set('watchingFile', true);
-
 	try {
 		var ringPath = RingPath(
 			faliename, 100
 		);
 		ringPaths[aSocket.id] = ringPath;
+		console.log("count started : ", Object.keys(ringPaths).length);
 		ringPath.on('progress',
 			function (aId, aName) {
 				aSocket.emit('liveStream', aName + '?_t=' + (Math.random() * 100000), aSocket.id + " " + aId);
