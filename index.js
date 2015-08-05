e9ca9a221d4cd0d4ec1dbd5aecdfa257933a38db
gitseitanaka <gitseitanaka@users.noreply.github.com>
2015-08-05 15:56:51 +0900

    Revert "* split css file."
    This reverts commit 87c4cfa5440425e11fdc697a556a3e1ffb5b363a.

--- index.js
+++ index.js
@@ -5,19 +5,15 @@ var app = express();
 var http = require('http').Server(app);
 var path = require('path');
 
-var paths = {
-	index  : path.join(__dirname, 'view/index.html'),
-	stream : path.join(__dirname, 'stream'),
-	view   : path.join(__dirname, 'view'),
-};
+var stream_path = path.join(__dirname, 'stream');
 
 require('./lib/routes.js')(
 				express, app,
-				paths);
+				stream_path, __dirname + '/index.html');
 
 require('./lib/socketdispatch.js')(
 				http,
-				path.join(paths.stream, 'path.txt'));
+				path.join(stream_path, 'path.txt'));
 
 http.listen(3000, function () {
 	console.log('listening on *:3000');
