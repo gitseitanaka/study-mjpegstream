d83735bfc2b5f36e8f81a748e251a1430badbc40
gitseitanaka <gitseitanaka@users.noreply.github.com>
2015-08-06 10:05:18 +0900

    * tentatively..

--- view/js/slider.js
+++ view/js/slider.js
@@ -7,54 +7,56 @@
   var dragging = false;
   var width = input.clientWidth / 2;
 
-  var TICK = slider.clientWidth / 5000;
   var MIN_VALUE = 100;
+  var TICK = (1000 - MIN_VALUE) / slider.clientWidth;
 
   var set_value = function (aRealValue){
-    // つまみのサイズ(input.clientWidth)だけ位置を調整
+    // calc slider size
     input.style.left = (aRealValue - input.clientWidth/2) + 'px';
 
-    output.value =Math.round((TICK * aRealValue) + 100);
-    //output.value = output.value > MIN_VALUE ? output.value : MIN_VALUE;
-
+    output.value =Math.round((TICK * aRealValue) + MIN_VALUE);
   };
   set_value(0);
 
-  // 目盛り部分をクリックしたとき
+  // On key up in the TextBox.
+  output.onkeyup = function (evt) {
+    //console.log(output.value);
+  };
+
+  // On click at the Scale.
   slider.onclick = function(evt){
     dragging = true;
     document.onmousemove(evt);
     document.onmouseup();
   };
-  // ドラッグ開始
+  // Started to drag the mouse.
   input.onmousedown = function(evt){
     dragging = true;
-    return false;
   };
-  // ドラッグ終了
+  // Ended to drag th mouse.
   document.onmouseup = function(evt){
     if (dragging) {
       dragging = false;
     }
   };
+  // Dragging the mouse.
   document.onmousemove = function(evt){
-    if(dragging){
-      // ドラッグ途中
+    if(dragging){ // now Dragging..
       if(!evt){
         evt = window.event;
       }
       var left = evt.clientX;
       var rect = slider.getBoundingClientRect();
-      // マウス座標とスライダーの位置関係で値を決める
+      // calc slider position.
       var real_value = Math.round(left - rect.left - width);
-      // スライダーからはみ出したとき
+
+      // when over slider's width..
       if (real_value < 0 ) {
         real_value = 0;
       } else if (real_value > slider.clientWidth) {
         real_value = slider.clientWidth;
       }
       set_value(real_value);
-      return false;
     }
   };
 })();
